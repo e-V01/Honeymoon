7b5cccec6f30af840084dea2a9b96001009f3a1a
e-V01 <91158407+e-V01@users.noreply.github.com>
2024-04-11 12:55:02 +0300

    CardView set

---
+++ Honeymoon/View/CardView.swift
@@ -0,0 +1,54 @@
+//
+//  CardView.swift
+//  Honeymoon
+//
+//  Created by Y K on 11.04.2024.
+//
+
+import SwiftUI
+
+struct CardView: View {
+    let id = UUID()
+    var honeymoon: Destination
+    
+    var body: some View {
+        Image(honeymoon.image)
+            .resizable()
+            .clipShape(RoundedRectangle(cornerRadius: 24))
+            .scaledToFit()
+            .frame(minWidth: 0, maxWidth: .infinity)
+            .overlay(alignment: .bottom) {
+                VStack(alignment: .center, spacing: 12) {
+                    Text(honeymoon.place.uppercased())
+                        .foregroundStyle(Color.white)
+                        .font(.largeTitle)
+                        .fontWeight(.bold)
+                        .shadow(radius: 1)
+                        .padding(.horizontal, 18)
+                        .padding(.vertical, 4)
+                        .overlay(alignment: .bottom) {
+                            Rectangle()
+                                .fill(Color.white)
+                                .frame(height: 1)
+                        }
+                    Text(honeymoon.country.uppercased())
+                        .foregroundStyle(Color.black)
+                        .font(.footnote)
+                        .fontWeight(.bold)
+                        .frame(minWidth: 85)
+                        .padding(.horizontal, 10)
+                        .padding(.vertical, 5)
+                        .background(
+                            Capsule().fill(Color.white)
+                        )
+
+                }
+                .frame(minWidth: 280)
+                .padding(.bottom, 50)
+            }
+    }
+}
+
+#Preview {
+    CardView(honeymoon: honeymoonData[1])
+}
