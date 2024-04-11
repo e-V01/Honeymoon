d7cb6d490d5771e7827fadeb457d653d53ce0231
e-V01 <91158407+e-V01@users.noreply.github.com>
2024-04-11 12:32:16 +0300

    created model for Honeymoon , data imported

---
+++ Honeymoon/Model/HoneymoonModel.swift
@@ -0,0 +1,14 @@
+//
+//  HoneymoonModel.swift
+//  Honeymoon
+//
+//  Created by Y K on 11.04.2024.
+//
+
+import SwiftUI
+
+struct Destination {
+    var place: String
+    var country: String
+    var image: String
+}
