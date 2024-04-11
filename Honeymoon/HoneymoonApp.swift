a6c1a55b834d005683171cd6b3e6d0ec3cae665c
e-V01 <91158407+e-V01@users.noreply.github.com>
2024-04-11 12:10:01 +0300

    Initial Commit

---
+++ Honeymoon/HoneymoonApp.swift
@@ -0,0 +1,17 @@
+//
+//  HoneymoonApp.swift
+//  Honeymoon
+//
+//  Created by Y K on 11.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct HoneymoonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
