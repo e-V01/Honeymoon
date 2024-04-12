8cd5954e0e25828f7c62fd289fcba2775ced7c2c
e-V01 <91158407+e-V01@users.noreply.github.com>
2024-04-12 17:23:04 +0300

    CardView conforms now ot identifiable comput prop for cardViews in array of CardView created looped for in to display only 2 cards, dismissable and next one, isTopCard to disaply firstIndex of it

--- Honeymoon/View/CardView.swift
+++ Honeymoon/View/CardView.swift
@@ -7,7 +7,7 @@
 
 import SwiftUI
 
-struct CardView: View {
+struct CardView: View, Identifiable {
     let id = UUID()
     var honeymoon: Destination
     
@@ -50,5 +50,5 @@ struct CardView: View {
 }
 
 #Preview {
-    CardView(honeymoon: honeymoonData[1])
+    CardView(honeymoon: honeymoonData[0])
 }
--- Honeymoon/View/ContentView.swift
+++ Honeymoon/View/ContentView.swift
@@ -11,17 +11,43 @@ struct ContentView: View {
     @State var showAlert: Bool = false
     @State var showGuide: Bool = false
     @State var showInfo: Bool = false
+    
+    // MARK: - Card views
+    var cardViews: [CardView] = {
+        var views = [CardView]()
+        for i in 0..<2  {
+            views.append(CardView(honeymoon: honeymoonData[i]))
+        }
+        return views
+    }()
+    
+    // MARK: - Top Card (if card is top most one or not)
+    private func isTopCard(cardView: CardView) -> Bool {
+        guard let index = cardViews.firstIndex(where: {$0.id == cardView.id}) else {
+            return false
+        }
+        return index == 0
+    }
 
     
     var body: some View {
         VStack {
-            HeaderView(showGuideView: $showGuide, 
+            // MARK: - Header
+            HeaderView(showGuideView: $showGuide,
                        showInfoView: $showInfo)
             Spacer()
-            CardView(honeymoon: honeymoonData[0])
-            // fixed: add apdding
-                .padding()
+            
+            // MARK: - Cards
+            ZStack {
+                ForEach(cardViews) { cardView in
+                    cardView
+                        .zIndex(self.isTopCard(cardView: cardView) ? 1 : 0)
+                }
+            }
+            .padding(.horizontal)
             Spacer()
+            
+            // MARK: - Footer
             FooterView(showBookingAlert: $showAlert)
         }
         .alert(isPresented: $showAlert) {
